973c43a51ee10e556d89b2480e2abba427d01f11
nn774 <admin@nn774tg.ru>
2024-06-02 18:43:09 +0500

    sa

--- windows/AuthWindow.xaml.cs
+++ windows/AuthWindow.xaml.cs
@@ -24,33 +24,6 @@ namespace demoPP.windows
         public AuthWindow()
         {
             InitializeComponent();
-            TradeContext tradeContext = new TradeContext();
-            List<Product> products = tradeContext.Products.ToList();
-            foreach (var product in products)
-            {
-                byte[] photoBytes = File.ReadAllBytes("C:\\Users\\User\\Desktop\\09_1.6-2022_10\\Вариант 10\\Вариант 10\\Сессия 1\\picture.png");
-
-                bool tt = false;
-                foreach (var file in Directory.GetFiles("C:\\Users\\User\\Desktop\\09_1.6-2022_10\\Вариант 10\\Вариант 10\\Сессия 1\\Товар_import"))
-                {
-
-                    if (File.Exists(file))
-                    {
-                        if (product.ProductArticleNumber == file.Replace("C:\\Users\\User\\Desktop\\09_1.6-2022_10\\Вариант 10\\Вариант 10\\Сессия 1\\Товар_import\\", "").Replace(".jpg", "").Replace(".jpeg", ""))
-                        {
-                            byte[] photoByte = File.ReadAllBytes(file);
-                            product.ProductPhoto = photoByte;
-                            tt = true;
-                        }
-                    }
-                }
-                if (!tt)
-                {
-                    product.ProductPhoto = photoBytes;
-                    tt = false;
-                }
-            }
-            tradeContext.SaveChanges();
         }
     }
 }
